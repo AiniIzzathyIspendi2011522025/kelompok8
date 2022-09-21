3bd3126699e1a12e3e2bbdaabc74114a6c21f8c2
AiniIzzathyIspendi2011522025 <90383696+AiniIzzathyIspendi2011522025@users.noreply.github.com>
2022-09-21 17:55:25 +0700

    progress1 - layout

---
+++ app/src/main/java/com/example/kelompok8/LaporanKP.kt
@@ -0,0 +1,11 @@
+package com.example.kelompok8
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LaporanKP : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_laporan_kp)
+    }
+}
