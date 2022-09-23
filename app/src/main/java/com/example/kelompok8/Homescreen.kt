018ff68c3afd964c12b3cdbac74587c734c2f2a2
AiniIzzathyIspendi2011522025 <90383696+AiniIzzathyIspendi2011522025@users.noreply.github.com>
2022-09-23 15:03:15 +0700

    progress - layout (onclick1)

--- app/src/main/java/com/example/kelompok8/Homescreen.kt
+++ app/src/main/java/com/example/kelompok8/Homescreen.kt
@@ -1,11 +1,33 @@
 package com.example.kelompok8
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class Homescreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_homescreen)
+    }
+
+    fun onButtonPengajuanKP(view: View) {
+        intent = Intent(this, ListPengajuanKP::class.java)
+        startActivity(intent)
+    }
+
+    fun onButtonDetailKP(view: View) {
+        intent = Intent(this, DetailKP::class.java)
+        startActivity(intent)
+    }
+
+    fun onButtonHome(view: View) {
+        intent = Intent(this, Homescreen::class.java)
+        startActivity(intent)
+    }
+
+    fun onButtonProfil(view: View) {
+        intent = Intent(this, ProfilPengguna::class.java)
+        startActivity(intent)
     }
 }
